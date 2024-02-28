5e57489234a16c84ef836114e6b19301c5b44fb7
Mitch Marsh <NewCoder2024@gmail.com>
2024-02-28 15:56:10 -0700

    object1

---
+++ functions-2-demo/starter.js
@@ -0,0 +1,73 @@
+////////////////////////
+////// CALCULATOR //////
+////////////////////////
+
+// CODE HERE
+
+
+///////////////////////
+////// PET STORE //////
+///////////////////////
+
+const dogProducts = [
+    {
+      name: 'leash',
+      colors: ['red', 'blue', 'green'],
+      category: 1,
+      inventory: 30,
+      basePrice: 13.99, 
+      displayPrice: 13.99
+    }, 
+    {
+      name: 'chew toy',
+      colors: ['brown'],
+      category: 2,
+      inventory: 120,
+      basePrice: 6.00, 
+      displayPrice: 6.00
+    }, 
+    {
+      name: 'rope',
+      colors: ['blue & green', 'red & yellow'],
+      category: 2,
+      inventory: 75,
+      basePrice: 4.99, 
+      displayPrice: 4.99
+    }
+]
+
+const catProducts = [
+  {
+    name: 'mouse toy', 
+    colors: ['pink', 'grey', 'black'], 
+    category: 2, 
+    inventory: 125, 
+    basePrice: 2.50, 
+    displayPrice: 2.50
+  },
+  {
+    name: 'cat sweater',
+    colors: ['black'],
+    category: 1,
+    inventory: 15,
+    basePrice: 10.00, 
+    displayPrice: 10.00
+  }, 
+  {
+    name: 'straching post',
+    colors: ['tan'],
+    category: 2,
+    inventory: 40,
+    basePrice: 22.99, 
+    displayPrice: 22.99
+  }
+]
+
+// CODE HERE
+
+
+////////////////////////
+////// SANDWICHES //////
+////////////////////////
+
+// CODE HERE
